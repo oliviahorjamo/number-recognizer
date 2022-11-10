29ae2acc89bc567c9cc8dccb781a2856de35ae4e
Olivia Horjamo <olivia.horjamo@gmail.com>
2022-11-10 17:36:50 +0200

    Implement tests for layer size and number range

--- src/tests/layer_test.py
+++ src/tests/layer_test.py
@@ -9,9 +9,17 @@ class StubLayer:
 class TestLayer(unittest.TestCase):
 
     def setUp(self):
-        self.input_size = 28*28
-        self.layer_size = 28*28
+        self.input_size = 3
+        self.layer_size = 4
         self.layer = Layer(self.input_size, self.layer_size)
 
-    def test_init_layer_size(self):
+    def test_init_layer_weights_size(self):
         self.assertEqual(self.layer.weights.size, self.input_size*self.layer_size)
+
+    def test_init_layer_biases_size(self):
+        self.assertEqual(self.layer.biases.size, 1*self.layer_size)
+
+    def test_weights_within_range(self):
+        within_range = np.where(np.logical_and(self.layer.weights >= 0, self.layer.weights <= 1))
+        self.assertEqual(within_range[0].size, self.layer.weights.size)
+

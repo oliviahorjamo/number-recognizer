de0035c0636a6cc4f37abb1af74e62a4178c7076
Olivia Horjamo <olivia.horjamo@gmail.com>
2022-11-17 17:19:17 +0200

    Fix activation function bug
    The previous activation function stopped working with an input
    with both positive and negative numbers, but I couln't find the cause
    of the issue. The new function works with all inputs.

--- src/neural_network/activation_functions.py
+++ src/neural_network/activation_functions.py
@@ -4,7 +4,9 @@ import numpy as np
 def softmax_item(item):
     return max(0, item)
 
-def softmax(input_data):
+def softmax_old(input_data):
     func = np.vectorize(softmax_item)
     return func(input_data)
     
+def relu(input_data):
+    return (np.maximum(0, input_data))
--- src/neural_network/layer.py
+++ src/neural_network/layer.py
@@ -29,24 +29,42 @@ class Layer:
     def dot_product(self, input_data):
         return np.dot(input_data, self.weights)
 
+    def dot_product_transpose(self, input_data):
+        return np.dot(input_data, self.weights.T)
+
     def add_biases(self, input_data):
         return input_data + self.biases
+    
+    def activation(self, input_array):
+        return activation_functions.relu(input_array)
 
-    def forward_propagation(self, input_data):
-        """y = b + x*w where y, b and x are vectors and w is a weight matrix
-            for the edges between the previous layer and this layer
+    def forward_propagation(self, input_array):
+        """Propagate data forwards: calculate the output of this layer by calculating the
+        dot product of the input data and the weights between this layer and the previous
+        layer and add biases. Save input_data as self.input since this information will be
+        needed later in back propagation.
 
         Parameters:
         input_data: numpy array
             the output of the previous layer
         """
-        x_dot_weigths = self.dot_product(input_data)
+        self.input = input_array
+        x_dot_weigths = self.dot_product(input_array)
         x_plus_biases = self.add_biases(x_dot_weigths)
         x_final = self.activation(x_plus_biases)
         return x_final
 
-    def backward_propagation(self):
-        raise NotImplementedError
+    def backward_propagation(self, output_error):
+        """Based on the error with respect to the output of this layer, calculate the error
+        with respect to the weights and biases of this layer and then output the error with
+        respect to the input of this layer. This will yield the output_error for the previous
+        layer.
+        
+        Parameters:
+        output_error: the derivate of the error with respect to this layer's output
 
-    def activation(self, input_array):
-        return activation_functions.softmax(input_array)
+        Outputs:
+        the derviate of the error with respect to the input of this layer
+        
+        """
+        input_error = self.dot_product_transpose(output_error)

6cddc8b48651dc257b36b3ab8e0a802efa780d57
Olivia Horjamo <olivia.horjamo@gmail.com>
2022-12-01 16:05:11 +0200

    Fix the calculation of the mean squared error

--- src/neural_network/loss_functions.py
+++ src/neural_network/loss_functions.py
@@ -8,17 +8,4 @@ def mean_squared_error(y_true, y_pred):
     y_true: the expected output of the layer
     y_pred: the actual output of the layer
     """
-    error_vector = (y_true - y_pred) * (y_true - y_pred)
-    error = 1 / len(y_true) * sum(error_vector)
-    return error
-
-def mse_gradient(y_true, y_pred):
-    """Calculate the gradient of the error with respect to the output. Gradient
-    tells the derivative of the error with respect to each neuron i.e., tells how
-    wrong the output of each neuron is.
-    """
-    error_vector = y_true - y_pred
-    print('error vector')
-    print(error_vector)
-    gradient = 2 / len(y_true) * error_vector
-    return gradient
+    return np.mean(np.power(y_true-y_pred, 2))

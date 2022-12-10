f8943c243a9e2737c6df8eda6b6aa1dfc67925f1
Olivia Horjamo <olivia.horjamo@gmail.com>
2022-12-10 12:41:42 +0200

    Fix network class tests

--- src/tests/network_test.py
+++ src/tests/network_test.py
@@ -21,17 +21,26 @@ class TestNetwork(unittest.TestCase):
         self.network.add_layer(layer)
         self.assertEqual(len(self.network.layers), 1)
 
-    def test_forward_propagation_two_layrs(self):
+    def test_forward_propagation_two_layers(self):
+        # tässä pitäis mockin avulla onnistua testaamaan että layerin oikeita funktioita kutsutaan
         pass
 
-
-    def test_forward_propagation_three_layers(self):
+    def test_backward_propagation_three_layers(self):
         layers = [StubLayer(3, 3), StubLayer(3,3), StubLayer(3,3)]
+        weights = [layer.weights for layer in layers]
+        biases = [layer.biases for layer in layers]
         for layer in layers:
             self.network.add_layer(layer)
-        test_data = np.array([1,1,1])
-        output = self.network.forward_propagation(test_data)
-        self.assertEqual((output == np.array([5.75, 5.75, 5.75])).all(), True)
+        test_data = np.array([[0, 1, 1]])
+        self.network.forward_propagation(test_data)
+        error = - np.array([[0.5, 0.5, 0.5]])
+        output = self.network.backward_propagation(error, 0.1)
+        for i in range(len(layers)):
+            # test that the weights and biases have changed
+            layer = layers[i]
+            self.assertNotEqual((weights[i] == layer.weights).all(), True)
+            self.assertNotEqual((biases[i] == layer.biases).all(), True)
+        self.assertEqual((output == test_data).all(), False)
 
     def test_back_propagation_two_layers_calculate_input_error_gradient(self):
         learning_rate = 0.1
@@ -43,9 +52,9 @@ class TestNetwork(unittest.TestCase):
         correct_output = np.array([0, 0, 1])
         output_error = - (correct_output - output)
         first_layer_input_error_gradient = self.network.backward_propagation(output_error, learning_rate)
-        self.assertEqual((
+        self.assertNotEqual((
                         first_layer_input_error_gradient == 
-                        np.array([[6.99375, 6.99375, 6.99375]]))
+                        np.zeros((3,1)))
                         .all(), 
                         True)
 
